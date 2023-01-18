e659fdc13590e03411116588debc73be60eb2670
Perming1126 <perming@student.usm.my>
2023-01-18 22:35:53 +0800

    Prepare File structure

---
+++ src/main/java/com/example/cat201_project/homePageController.java
@@ -0,0 +1,14 @@
+package com.example.cat201_project;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class homePageController {
+
+    @FXML Button btn1;
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
