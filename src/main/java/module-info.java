7db24f855923f439f5d178541d05c65292dbbcb6
Perming1126 <perming@student.usm.my>
2023-01-18 20:52:21 +0800

    initialize repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cat201_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cat201_project to javafx.fxml;
+    exports com.example.cat201_project;
+}
