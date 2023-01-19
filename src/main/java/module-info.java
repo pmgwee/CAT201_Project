19912e61ae0d4417dbbcf87910fc7ad6b6a4df84
Perming1126 <perming@student.usm.my>
2023-01-19 21:03:29 +0800

    ticket history,login fxml file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.cat201_project {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.cat201_project to javafx.fxml;
     exports com.example.cat201_project;
 }
