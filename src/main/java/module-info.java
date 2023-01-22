8ce3ff457d78220c101b71410b2cd222f0ef5acc
User <waihong0717@gmail.com>
2023-01-22 19:02:54 +0800

    testing changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.cat201_project {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires javafx.controls;
+    requires javafx.fxml;
 
     opens com.example.cat201_project to javafx.fxml;
     exports com.example.cat201_project;
